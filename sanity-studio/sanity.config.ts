c5227735d5802e3e326f267e2610fee02ebedd87
kihyeoon <bangion93@gmail.com>
2023-05-18 23:46:24 +0900

    ➕ sanity 설치

---
+++ sanity-studio/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'salmon-squirrel',
+
+  projectId: 'd8ehbcqw',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
