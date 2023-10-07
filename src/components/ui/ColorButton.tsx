1914b2d70bbd30183058c73b44adf517ff7187f5
kihyeoon <bangion93@gmail.com>
2023-10-07 22:54:49 +0900

    💄 ColorButton 테두리 height 설정

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function ColorButton({ text, onClick, size = "small" }: Props) {
   return (
-    <div className="rounded bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.1rem]">
+    <div className="rounded bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.1rem] h-fit">
       <button
         className={`bg-white hover:opacity-90 rounded-sm transition-opacity ${
           size === "large" ? "text-xl py-2 px-4" : "text-base py-1 px-2"
