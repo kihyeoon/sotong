404b718b7030f7a5c1cb6b606d6badbb0342b888
kihyeoon <bangion93@gmail.com>
2023-10-15 23:58:58 +0900

    ✨ 사용자 검색 - 유저 카드 컴포넌트 구현

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import UserCard from "@/components/UserCard";
 import GridSpinner from "@/components/ui/GridSpinner";
 import { ProfileUser } from "@/model/user";
 import { FormEvent, useState } from "react";
@@ -12,7 +13,6 @@ export default function UserSearch() {
     isLoading,
     error,
   } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
-  console.log("data", users);
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,9 +20,10 @@ export default function UserSearch() {
   };
 
   return (
-    <>
-      <form onSubmit={onSubmit}>
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
         <input
+          className="w-full border border-gray-400 rounded-md p-3 text-xl"
           type="text"
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
@@ -33,14 +34,14 @@ export default function UserSearch() {
       {error && <p>Error: {error.message}</p>}
       {isLoading && <GridSpinner />}
       {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음</p>}
-      <ul>
+      <ul className="w-full p-4">
         {users &&
           users.map((user) => (
             <li key={user.username}>
-              <p>{user.name}</p>
+              <UserCard user={user} />
             </li>
           ))}
       </ul>
-    </>
+    </section>
   );
 }
