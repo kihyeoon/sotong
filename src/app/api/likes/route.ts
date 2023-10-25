89a808e4549fffa447884bd13eeba955bcc205bf
kihyeoon <bangion93@gmail.com>
2023-10-26 00:34:10 +0900

    ✨ 좋아요 - 라우트핸들러

---
+++ src/app/api/likes/route.ts
@@ -0,0 +1,23 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { likePost, unlikePost } from "@/service/post";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const { id, like } = await req.json();
+  if (!id || like === undefined) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const request = like ? likePost : unlikePost;
+
+  return request(id, user.id)
+    .then((res) => NextResponse.json(res))
+    .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+}
