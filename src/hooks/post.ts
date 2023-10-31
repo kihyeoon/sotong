2b33200b08a765eabcd9291e92c0f97e9ae1b125
kihyeoon <bangion93@gmail.com>
2023-10-31 23:07:42 +0900

    ✨ 좋아요 - 낙관적 UI 업데이트

--- src/hooks/post.ts
+++ src/hooks/post.ts
@@ -1,17 +1,39 @@
 import { SimplePost } from "@/model/post";
 import useSWR, { useSWRConfig } from "swr";
 
-const API_URL = "/api/posts";
+const POST_URL = "/api/posts";
+const LIKE_URL = "/api/likes";
+
+async function updateLike(id: string, like: boolean) {
+  return fetch(LIKE_URL, {
+    method: "PUT",
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
 
 export default function usePost() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>(API_URL);
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>(POST_URL);
 
   const setLike = (post: SimplePost, username: string, like: boolean) => {
-    fetch(API_URL, {
-      method: "PUT",
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate("/api/posts"));
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter((name) => name !== username),
+    };
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
   };
 
   return {
