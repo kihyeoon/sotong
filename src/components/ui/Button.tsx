c78a358df32ca904f23c538f50373b8289cc5576
kihyeoon <bangion93@gmail.com>
2023-11-12 23:54:45 +0900

    ✨ SSR 페이지 업데이트 - useTransition, router.refresh 사용

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,15 +2,22 @@ interface Props {
   text: string;
   onClick: () => void;
   isRed?: boolean;
+  disabled?: boolean;
 }
 
-export default function Button({ text, onClick, isRed }: Props) {
+export default function Button({
+  text,
+  onClick,
+  isRed,
+  disabled = false,
+}: Props) {
   return (
     <button
       onClick={onClick}
       className={`border-none rounded-md py-2 px-8 text-white font-bold leading-4 ${
         isRed ? "bg-red-500" : "bg-sky-500"
-      }`}
+      } ${disabled && "opacity-50 cursor-not-allowed"}`}
+      disabled={disabled}
     >
       {text}
     </button>
--- src/components/ui/FollowButton.tsx
+++ src/components/ui/FollowButton.tsx
@@ -2,11 +2,17 @@
 import Button from "@/components/ui/Button";
 import useMe from "@/hooks/me";
 import { ProfileUser } from "@/model/user";
-import { follow } from "@/service/user";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { PulseLoader } from "react-spinners";
 
 export default function FollowButton({ user }: { user: ProfileUser }) {
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
 
   const showButton = loggedInUser && loggedInUser.username !== username;
   const isFollowing =
@@ -14,18 +20,31 @@ export default function FollowButton({ user }: { user: ProfileUser }) {
     loggedInUser.following.find((item) => item.username === username);
   const text = isFollowing ? "Unfollow" : "Follow";
 
-  const handleFollow = () => {
-    toggleFollow(user.id, !isFollowing);
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !isFollowing);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
   };
 
   return (
     <>
       {showButton && (
-        <Button
-          text={text}
-          onClick={handleFollow}
-          isRed={text === "Unfollow"}
-        />
+        <div className="relative">
+          {isUpdating && (
+            <div className="absolute inset-0 flex justify-center items-center z-10">
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button
+            text={text}
+            onClick={handleFollow}
+            isRed={text === "Unfollow"}
+            disabled={isUpdating}
+          />
+        </div>
       )}
     </>
   );
