866a634f79e5a1206754f92797f310c73dc891f0
kihyeoon <bangion93@gmail.com>
2023-11-19 14:08:19 +0900

    💄 새포스트 작성 - UI

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -3,6 +3,7 @@ import PostUserAvatar from "@/components/PostUserAvatar";
 import Button from "@/components/ui/Button";
 import FilesIcon from "@/components/ui/icons/FilesIcon";
 import { AuthUser } from "@/model/user";
+import Image from "next/image";
 import { ChangeEvent, DragEvent, useState } from "react";
 
 interface Props {
@@ -44,9 +45,9 @@ export default function NewPost({ user: { username, image } }: Props) {
   };
 
   return (
-    <section>
+    <section className="w-full max-w-xl flex flex-col items-center mt-6 mx-1">
       <PostUserAvatar username={username} userImage={image ?? ""} />
-      <form>
+      <form className="w-full flex flex-col mt-2 gap-4">
         <input
           className="hidden"
           name="input"
@@ -56,17 +57,37 @@ export default function NewPost({ user: { username, image } }: Props) {
           onChange={handleChange}
         />
         <label
+          className={`w-full h-60 flex flex-col items-center justify-center ${
+            !file && "border-2 border-sky-500 border-dashed"
+          }`}
           htmlFor="input-upload"
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDragOver}
           onDrop={handleDrop}
         >
-          <FilesIcon />
-          <p>Drag and Drop your image here or click</p>
+          {dragging && (
+            <div className="absolute inset-0 bg-sky-500/20 pointer-events-none" />
+          )}
+          {!file ? (
+            <div className="flex flex-col items-center pointer-events-none">
+              <FilesIcon />
+              <p>Drag and Drop your image here or click</p>
+            </div>
+          ) : (
+            <div className="relative w-full aspect-square">
+              <Image
+                className="object-cover"
+                src={URL.createObjectURL(file)}
+                alt="local file"
+                fill
+                sizes="650px"
+              />
+            </div>
+          )}
         </label>
         <textarea
-          className="w-full h-20 p-2 border border-gray-300 rounded-md"
+          className="outline-none resize-none text-lg border border-neutral-300"
           name="text"
           id="input-text"
           rows={10}
