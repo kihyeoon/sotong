e0e588b5875ffbdc699c786a50dd802293c418e0
kihyeoon <bangion93@gmail.com>
2023-11-20 00:57:01 +0900

    ✨ 새포스트 작성 - 서버

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getFollowingPostsOf } from "@/service/post";
+import { createPost, getFollowingPostsOf } from "@/service/post";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
@@ -12,6 +12,27 @@ export async function GET() {
   }
 
   return getFollowingPostsOf(user.username).then((posts) =>
+    NextResponse.json(posts)
+  );
+}
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+
+  const form = await req.formData();
+  const text = form.get("text")?.toString();
+  const file = form.get("file") as Blob;
+
+  if (!text || !file) {
+    return new Response("Missing text or file", { status: 400 });
+  }
+
+  return createPost(user.id, text, file).then((posts) =>
     NextResponse.json(posts)
   );
 }
